1df6cd364f0ebc86e59ff044c10a34dc704d2569
joyjsmun <joy.js.mun@gmail.com>
2022-11-27 02:16:16 +0900

    ADD: first commit

--- hardhat/scripts/constants/index.js
+++
@@ -1,5 +0,0 @@
-const WHITELIST_CONTRACT_ADDRESS = "0x32F6c0Cf26fdeCb710a7faEc81F1Ecc579940Bd1";
-
-const METADATA_URL = "https://nft-collection-self.vercel.app/api/";
-
-module.exports = {WHITELIST_CONTRACT_ADDRESS,METADATA_URL};
--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,32 +1,31 @@
-const {ethers} = require("hardhat");
-require("dotenv").config({path:".env"});
-const {WHITELIST_CONTRACT_ADDRESS,METADATA_URL} = require("./constants");
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
 
-async function main(){
-  //Address of the whitelist contract that I deployed in the previous module
-  const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
-  //URL from where it can extract the metadata for a NFT
-  const metadataURL = METADATA_URL;
+async function main() {
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  //A contractFactory in the ethers.js is an abstraction used to deploy new smart contracts,
-  //so cryptoDevsContract here is a factory for instances of our CryptoDevsContract.
-  const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
+  const lockedAmount = hre.ethers.utils.parseEther("1");
 
-  //deploy the contract
-  const deployedCryptoDevsContract = await cryptoDevsContract.deploy(
-    metadataURL,
-    whitelistContract
-  )
+  const Lock = await hre.ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+  await lock.deployed();
 
   console.log(
-    "Contract Address:",
-    deployedCryptoDevsContract.address
+    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
   );
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  })
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
