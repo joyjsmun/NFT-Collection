86801ed0fb540ba43e9c3558e013d6351e546c32
joyjsmun <joy.js.mun@gmail.com>
2022-11-28 15:30:42 +0900

    ADD: deploy setting

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xd67A9BDacA92b9679927cEf9860447C7E16fad6a";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
